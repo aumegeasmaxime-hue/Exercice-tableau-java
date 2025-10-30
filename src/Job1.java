2a81082fff4ebe4d2bf7cb6e0770427ef9ff2b0c
maxime <maxime.aumegeas@laplateforme.io>
2025-10-30 14:27:01 +0100

    creation job 1 tableau

---
+++ src/Job1.java
@@ -0,0 +1,36 @@
+public class Job1 {
+    public static void main (String[] args) {
+
+        int [] a = {200 , 204 , 173 , 98 , 171 , 404 ,459};
+
+        for ( int i : a) {
+
+            if ((i % 2) == 0) {
+                System.out.println(i + " est pair");
+            } else {
+                System.out.println(i + " est impair");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
