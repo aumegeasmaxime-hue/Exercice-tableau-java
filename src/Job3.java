c82fed69a87961c775ef4c90c587a3df1c1704c7
maxime <maxime.aumegeas@laplateforme.io>
2025-10-30 16:20:47 +0100

    ajout job 3

---
+++ src/Job3.java
@@ -0,0 +1,29 @@
+public class Job3 {
+    public void main(String[] args) {
+
+        String str = "I'm sorry Dave I'm afraid I can't do that";
+        char[] chars = str.toCharArray();
+
+        for (char i : chars) {
+
+            if ((i == 'a') || (i == 'e') || (i == 'i') ||(i == 'o') ||(i == 'u') || (i == 'y') || (i == 'I')) {
+                System.out.print(i);
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
