006700c3b861e37918fb5d410378bd7a1c610fba
maxime <maxime.aumegeas@laplateforme.io>
2025-10-30 15:44:19 +0100

    ajout job 2

---
+++ src/Job2.java
@@ -0,0 +1,16 @@
+public class Job2 {
+    public static void main (String[] args) {
+
+
+        String str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+
+        char[] chars = str.toCharArray();
+        System.out.println(chars.length);
+
+        for (int i = 0; i < chars.length; i++) {
+            if ((i % 2) == 0) {
+                System.out.print(chars[i]);
+            }
+        }
+    }
+}
