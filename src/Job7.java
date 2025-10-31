c4f25ca6e678e066b0fae7d6d71481e164a57095
maxime <maxime.aumegeas@laplateforme.io>
2025-10-31 13:04:55 +0100

    ajout job 7

---
+++ src/Job7.java
@@ -0,0 +1,12 @@
+public class Job7 {
+    public static void main (String[] args) {
+
+        String str = "Certaines choses changent, et d'autres ne changeront jamais.";
+        char[] char1 = str.toCharArray();
+
+        for (int i = 1 ; i < char1.length; i++) {
+            System.out.print(char1[i] + "C");
+           }
+        }
+    }
+
