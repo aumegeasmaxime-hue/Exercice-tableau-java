f08f89261d8c4a9136ab1ab26cb60cd2ea3c953f
maxime <maxime.aumegeas@laplateforme.io>
2025-11-03 15:27:58 +0100

    Add job9

---
+++ src/Job9.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Job9 {
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("entrer un numéro entier");
+        int enter = input.nextInt();
+        int [] tab = {4,5,7,9,11};
+        int som = 0;
+        for (int i = 1 ; i < enter ; i++) {
+            for (int div : tab) {
+                if (i % div == 0) {
+                    som += i ;
+                }
+            }
+        }
+        System.out.println(som);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
