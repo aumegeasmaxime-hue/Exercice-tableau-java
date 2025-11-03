3829124c8d6f890068423e71a59c4b0b325beb98
maxime <maxime.aumegeas@laplateforme.io>
2025-11-03 11:16:47 +0100

    fin job8

--- src/Job8.java
+++ src/Job8.java
@@ -3,35 +3,25 @@ public class Job8 {
 
         int [] arr = {84, 12, 1, 43, 5, 0};
         int [] arrey = new int[arr.length];
+        int p = 0;
         int highest = 0;
         for (int x : arr) {
             if (x > highest) {
                 highest = x;
             }
         }
-        for (int i = 0 ; i <= highest ; i++ ) {
+        for (int croissant = 0 ; croissant <= highest ; croissant++ ) {
             for (int x : arr) {
-                if (i == x) {
-                    System.out.print(i + " ");
-
-
-
-                    }
+                if (croissant == x) {
+                    System.out.print(croissant + " ");
+                    arrey [p] = croissant;
+                    p++;
                 }
             }
-
         }
-        for (int a : arrey) {
-            System.out.println(a);
+        for (int w : arrey) {
+            System.out.print(w + " ");
         }
-
-
-
-
-
-
-
-
-
     }
 }
+
